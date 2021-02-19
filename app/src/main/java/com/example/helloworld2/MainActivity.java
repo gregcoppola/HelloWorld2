063b9d9e0894d9f57a3d0b1c2f2471c62adccdf8
gregcoppola <58798856+gregcoppola@users.noreply.github.com>
2021-02-18 20:34:10 -0500

    Added Click

--- app/src/main/java/com/example/helloworld2/MainActivity.java
+++ app/src/main/java/com/example/helloworld2/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.helloworld2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int counter = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void on_click(View v){
+        TextView tv = findViewById(R.id.msg);
+
+        counter += 1;
+        tv.setText("counter: " + counter);
+
     }
 }
